ec39cb758d2def8669c7eccb70b2f054963d31c2
Dima Chizhevsky <dima.chizhevsky@gmail.com>
2021-02-21 16:23:46 +0200

    fix: removed redundant errors

--- ec.go
+++ ec.go
@@ -3,9 +3,7 @@ package crypto
 import "errors"
 
 var (
-	ErrValidation             = errors.New("validation error")
 	ErrKeypairAlreadyPresent  = errors.New("key pair is already present")
-	ErrInvalidKeysPoolDirPath = errors.New("invalid keys pool directory path")
 )
 
 const (
